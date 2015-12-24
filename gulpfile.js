ac5d28e8cc0c4984261453e8efecddc67e34a2f2
Sean McCambridge <seanmccambridgedesign@gmail.com>
2015-12-24 17:59:16 -0500

    got tooling all done and working. now with Nunjucks.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ const rename 		= require('gulp-rename')
 const postcss		= require('gulp-postcss');
 const precss 		= require('precss');
 const autoprefixer  = require('autoprefixer');
+
+const njRender		= require('gulp-nunjucks-render');
+const nj 			= njRender.nunjucks;
+
+const browserSync 	= require('browser-sync');
+const reload      	= browserSync.reload;
  
 gulp.task('scripts', () => {
 	return gulp.src('src/scripts/**/*.js')
@@ -20,7 +26,8 @@ gulp.task('scripts', () => {
 		.pipe(rename('scripts.min.js'))
 		.pipe(uglify())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'))
+		.pipe(reload({stream:true}));
 });
 
 gulp.task('styles', () => {
@@ -32,3 +39,30 @@ gulp.task('styles', () => {
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('dist'))
 });
+
+gulp.task('markup', () => {
+	nj.configure(['src/templates'], {watch: false});
+	return gulp.src('src/html/**/*.+(html|nj|nunjucks)')
+		.pipe(njRender())
+		.pipe(gulp.dest('dist'));
+})
+
+gulp.task('watch', function() {
+	gulp.watch('src/templates/**/*.+(html|nj|nunjucks)', ['markup', reload]);
+	gulp.watch('src/html/**/*.+(html|nj|nunjucks)', ['markup', reload]);
+	gulp.watch('src/styles/**/*.css', ['styles', reload]);
+	gulp.watch(['src/scripts/**/*.js'], ['scripts', reload]);
+	gulp.watch("*.html", reload);
+});
+
+gulp.task('sync', function() {
+	browserSync({
+		server: {
+			baseDir: "./dist/"
+		}
+	});
+});
+
+gulp.task('server', ['markup', 'styles', 'sync', 'scripts', 'watch']);
+
+gulp.task('default', ['markup', 'styles', 'scripts']);
