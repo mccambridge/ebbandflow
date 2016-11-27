500d138f29d1421e282f43b9388a9f25190a4c73
Sean McCambridge <seanmccambridgedesign@gmail.com>
2016-11-27 16:26:40 -0500

    simple footer

--- dist/app.js
+++ dist/app.js
@@ -56,6 +56,10 @@
 	var debounce = __webpack_require__(3);
 	var menuClickHandler = __webpack_require__(4);
 
+	var liame = ['74', '69', '6D', '67', '72', '61', '64', '79', '40', '6D', '65', '2E', '63', '6F', '6D'];
+	var enohp = ['28', '38', '34', '33', '29', '20', '32', '32', '34', '2D', '30', '37', '35', '39'];
+	var tseuqer = ['74', '69', '6D', '67', '72', '61', '64', '79', '40', '6D', '65', '2E', '63', '6F', '6D', '3F', '73', '75', '62', '6A', '65', '63', '74', '3D', '52', '65', '71', '75', '65', '73', '74', '20', '41', '63', '63', '65', '73', '73', '20', '54', '6F', '20', '50', '68', '6F', '74', '6F', '73', '26', '62', '6F', '64', '79', '3D', '48', '69', '2C', '20', '54', '69', '6D', '2E', '20', '49', '27', '64', '20', '6C', '69', '6B', '65', '20', '74', '6F', '20', '73', '65', '65', '20', '79', '6F', '75', '72', '20', '70', '68', '6F', '74', '6F', '20', '67', '61', '6C', '6C', '65', '72', '79', '2E'];
+
 	(function ($) {
 		var ebby = {
 			events: function events() {
@@ -68,6 +72,11 @@
 					$('.grid').masonry('layout');
 				});
 			},
+			decodeContactInto: function decodeContactInto() {
+				$('#liame').html('<a href="mailto:&#x' + liame.join(';&#x') + ';">&#x' + liame.join(';&#x') + ';</a>');
+				$('#enohp').html('&#x' + enohp.join(';&#x') + ';');
+				$('#tseuqer').html('<a href="mailto:&#x' + tseuqer.join(';&#x') + ';">Request access to full photo gallery</a>');
+			},
 			init: function init() {
 				var _this = this;
 
@@ -75,6 +84,7 @@
 					$('.lazy').lazyload();
 					deviceTweaks();
 					_this.events();
+					_this.decodeContactInto();
 					svg();
 				});
 			}
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,10 @@ const svg = require('./utils/svg.js');
 const debounce = require('./utils/debounce.js');
 const menuClickHandler = require('./events/menuClick.js');
 
+const liame = ['74','69','6D','67','72','61','64','79','40','6D','65','2E','63','6F','6D'];
+const enohp = ['28','38','34','33','29','20','32','32','34','2D','30','37','35','39'];
+const tseuqer = ['74','69','6D','67','72','61','64','79','40','6D','65','2E','63','6F','6D','3F','73','75','62','6A','65','63','74','3D','52','65','71','75','65','73','74','20','41','63','63','65','73','73','20','54','6F','20','50','68','6F','74','6F','73','26','62','6F','64','79','3D','48','69','2C','20','54','69','6D','2E','20','49','27','64','20','6C','69','6B','65','20','74','6F','20','73','65','65','20','79','6F','75','72','20','70','68','6F','74','6F','20','67','61','6C','6C','65','72','79','2E'];
+
 (($) => {
   const ebby = {
     events() {
@@ -18,11 +22,18 @@ const menuClickHandler = require('./events/menuClick.js');
       });
     },
 
+    decodeContactInto() {
+      $('#liame').html(`<a href="mailto:&#x${liame.join(';&#x')};">&#x${liame.join(';&#x')};</a>`);
+      $('#enohp').html(`&#x${enohp.join(';&#x')};`);
+      $('#tseuqer').html(`<a href="mailto:&#x${tseuqer.join(';&#x')};">Request access to full photo gallery</a>`);
+    },
+
     init() {
       $(document).ready(() => {
         $('.lazy').lazyload();
         deviceTweaks();
         this.events();
+        this.decodeContactInto();
         svg();
       });
     }
